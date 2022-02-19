ac21ef1b95fa5fd6083d78a73de771a1be08f6a9
daiyan.Shen <daishan526@gmail.com>
2022-02-19 23:23:53 +0800

    version 0.0.0

---
+++ utils/log/lib/index.js
@@ -0,0 +1,11 @@
+const log = require('npmlog');
+
+// 判断 debugger 模式
+log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info';
+
+log.heading = 'Dai'; // 修改前缀
+
+// 添加自定义
+log.addLevel('success', 2000, {fg: 'green', bold: true});
+
+module.exports = log;
